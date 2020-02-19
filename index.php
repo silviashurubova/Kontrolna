5c6234b405e595dcafcf768d501b0931cee9f54f
silvia <sisi270897@gmail.com>
2020-02-19 10:30:50 +0200

    added title

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html>
+<head>
+ <title>Our Project</title>
+</head>
 <body>
 
 <h1>Hello world! </h1>
@@ -7,5 +10,6 @@
 <p>My first paragraph.</p>
 <p>My second paragraph </p>
 <p>Have a nice day!</p>
+
 </body>
 </html>
